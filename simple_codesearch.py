3436f63c481ced54a5c416a8c864e7498f8ce13a
L. Mountrakis <LMountr@gmail.com>
2016-06-18 17:57:57 +0200

    Progress!

--- simple_codesearch.py
+++ simple_codesearch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, request, \
-    redirect, url_for, abort, session
+    redirect, url_for, abort, session, Markup
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'F34TF$($e34D'
@@ -16,18 +16,14 @@ def signup():
     from subprocess import Popen, PIPE
     session['username'] = request.form['username']
     session['message'] = Popen(
-        ['/usr/local/bin/ag', request.form['message']], stdout=PIPE
+        ['/usr/local/bin/ag', '--nogroup', request.form['query']], stdout=PIPE
     ).communicate()[0]  # request.form['message']
-    session['message'] = session['message'].replace('\n','<br/>')
-    return redirect(url_for('message'))
+    # session['message'] = session['message'].replace('\n','<br/>')
+    # session['message'] = session['message'].replace('\n', Markup('<br />'))
+    return render_template('index.html', username=session['username'],
+                           message=session['message'], query=request.form['query'])
+    # return redirect(url_for('message'))
 
-
-@app.route('/message')
-def message():
-    if 'username' not in session:
-        return abort(403)
-    return render_template('message.html', username=session['username'],
-                           message=session['message'])
 
 if __name__ == '__main__':
     app.run(debug=True)
