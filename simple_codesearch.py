18e08d64558ad57a769547875f788de4e7aa91f8
L. Mountrakis <LMountr@gmail.com>
2016-06-19 13:02:21 +0200

    Live search using jquery

--- simple_codesearch.py
+++ simple_codesearch.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
+from __future__ import print_function  # In python 2.7
+import sys
 from flask import Flask, render_template, request, \
-    redirect, url_for, abort, session, Markup
+    redirect, url_for, abort, session, Markup, jsonify
+from subprocess import Popen, PIPE
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'F34TF$($e34D'
+
+
+def search_source_code(query):
+    results = Popen(
+        ['/usr/local/bin/ag', '--group', request.form['query']],
+        # ['/usr/local/bin/ag', '--nogroup', request.form['query']],
+        stdout=PIPE
+    ).communicate()[0]
+    return results
 
 
 @app.route('/')
@@ -11,16 +23,24 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/_stuff', methods=['GET', 'POST'])
+def stuff():
+    # print(request.form['query'], file=sys.stderr)
+    if len(request.form['query']) > 3:
+        session['message'] = search_source_code(request.form['query'])
+    else:
+        session['message'] = ""
+    return jsonify(message=session['message'], query=request.form['query'])
+
+
 @app.route('/signup', methods=['POST'])
 def signup():
-    from subprocess import Popen, PIPE
-    session['message'] = Popen(
-        ['/usr/local/bin/ag', '--group', request.form['query']],
-        # ['/usr/local/bin/ag', '--nogroup', request.form['query']],
-        stdout=PIPE
-    ).communicate()[0]
-    return render_template('index.html', message=session['message'],
-                           query=request.form['query'])
+    if request.method == 'POST':
+        session['message'] = search_source_code(request.form['query'])
+        return render_template('index.html', message=session['message'],
+                               query=request.form['query'])
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
